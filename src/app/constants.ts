a3fd977ca0b0263dfa27614befe5ca93377e0794
Byoungcheon <lbcsultan@gmail.com>
2024-09-30 18:30:25 +0900

    2nd

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,7 +2,8 @@ import { getContract } from "thirdweb";
 import { client } from "./client";
 import { sepolia } from "thirdweb/chains";
 
-const contractAddress = "0x0d895F8F933EB54D6Ea0B29D80e4a05c9B8ae6f0";
+// const contractAddress = "0x0d895F8F933EB54D6Ea0B29D80e4a05c9B8ae6f0";
+const contractAddress = "0xe157287cAad17BA42439cCf14f15E93B9623cFF1";
 
 export const CONTRACT = getContract({
   client: client,
